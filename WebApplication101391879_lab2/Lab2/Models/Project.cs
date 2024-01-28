63f39460808b0b50a2e7df9d1661cbae66675672
FlagelloDiDio <99625578+anarsamadzade@users.noreply.github.com>
2024-01-28 18:58:10 -0500

    lab1, lab2 lab3
    lab1, lab2 lab3
    
    Co-Authored-By: FarzinRasooliGeorge <155836623+FarzinRasooliGeorge@users.noreply.github.com>
    Co-Authored-By: ssantilli <11447093+ssantilli@users.noreply.github.com>

---
+++ WebApplication101391879_lab2/Lab2/Models/Project.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Project
+{
+    public int ProjectId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public string? ProjectStatus { get; set; }
+
+    public Project(int projectId, string name, string description)
+    {
+        ProjectId = projectId;
+        Name = name;
+        Description = description;
+    }
+}
